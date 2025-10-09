1df89a4ac1ab0112f9fc8a1ac242b79d7d93b2c5
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2025-10-09 10:53:12 +0200

    Add comments in the setup script

--- Raspberry_Pi/setup/setup.sh
+++ Raspberry_Pi/setup/setup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Enable i2c + uart
+# Activer l'I2C
 sudo raspi-config nonint do_i2c 0
+# Activer le port série
 sudo raspi-config nonint do_serial_hw 0
+# Pas de console sur le port série
 sudo raspi-config nonint do_serial_cons 1
 
 sudo apt update
 sudo apt install mosquitto python3-pip -y
 
+# Installation de Mosquitto (serveur MQTT)
 cd /home/rinkin/setup 
 sudo cp -u mosquitto.conf /etc/mosquitto/conf.d/
 sudo systemctl restart mosquitto
 
+# Installation de Mediamtx (caméra)
 cd /home/rinkin/setup/mediamtx
 sudo cp -u mediamtx /usr/bin/
 sudo cp -u mediamtx.yml /etc/
@@ -21,6 +25,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable mediamtx.service
 sudo systemctl start mediamtx.service
 
+# Installation du programme en python
 cd /home/rinkin/rinkin
 python3 -m venv .venv
 ls .venv/bin
