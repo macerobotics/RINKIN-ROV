5d763ab4be7a10ee75b3eccf4c0a2e1f305ae2fc
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2025-12-19 11:59:23 +0100

    Improve the build system

--- rinkin-ui/docker/build.sh
+++ rinkin-ui/docker/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker build -t rinkin .
+pushd ../../..
+pwd
+ls
+docker build -t rinkin -f RINKIN-ROV/rinkin-ui/docker/Dockerfile .
 docker create --name tmp rinkin
-docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin-linux-x86_64 .
-docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin.exe .
+docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin-linux-x86_64 RINKIN-ROV/rinkin-ui/docker
+docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin.exe RINKIN-ROV/rinkin-ui/docker
+docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin-raspberry-pi RINKIN-ROV/rinkin-ui/docker
 docker rm tmp
+
+popd
