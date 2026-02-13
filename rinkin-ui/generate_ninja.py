735f0c61e83cecb1fa13cb72bab16aeefc101f9f
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2026-02-13 18:40:25 +0100

    Improve ninja generator

--- rinkin-ui/generate_ninja.py
+++ rinkin-ui/generate_ninja.py
@@ -3,14 +3,49 @@ import os
 import sys
 import glob
 
-imgui_dir = "lib/imgui"
-raylib_dir = "lib/raylib/src"
-rlimgui_dir = "lib/rlImGui"
-implot_dir = "lib/implot"
-lua_dir = "lib/lua-5.4.8/src"
-ffmpeg_dir = "lib/ffmpeg-8.0"
-zlib_dir = "lib/zlib"
-ffmpeg_configure_flags = " ".join([
+
+# TODO:
+# - strip in release mode
+# - debug / release for linux
+# - put back -j
+
+targets = {
+    "linux": {
+        "exe": "rinkin",
+        "ffmpeg-extra-flags": [],
+        "ldflags": [],
+        "lua-target": "linux",
+    },
+    "windows": {
+        "exe": "rinkin.exe",
+        "prefix": "x86_64-w64-mingw32-",
+        "ffmpeg-extra-flags": [
+            '--target-os=mingw32',
+	        '--arch=x86_64',
+	        '--enable-cross-compile',
+	        '--cross-prefix=x86_64-w64-mingw32-'
+        ],
+        "ldflags": [
+            "-lws2_32",
+            "-lgdi32",
+            "-lopengl32",
+            "-lwinmm",
+            "-lncrypt",
+            "-lsecur32",
+            "-lcrypt32",
+        ],
+        "lua-target": "mingw"
+    },
+    "rpi": {
+        "exe": "rinkin",
+        "ffmpeg-extra-flags": [],
+        "prefix": "aarch64-linux-gnu-",
+        "ldflags": [],
+        "lua-target": "linux"
+    }
+}
+
+ffmpeg_configure_flags = [
     '--disable-encoders',
 	'--disable-hwaccels',
 	'--disable-muxers',
@@ -38,54 +73,105 @@ ffmpeg_configure_flags = " ".join([
 	'--disable-bzlib',
 	'--extra-cflags="-Os -fPIC"',
 	'--extra-ldflags="-s"',
-])
+]
 
-libraylib_a = raylib_dir + "/libraylib.a"
-liblua_a = lua_dir + "/liblua.a"
-libz_a = "build/zlib/libz.a"
+libs = [
+    "raylib",
+    "lua-5.4.8",
+    "ffmpeg-8.0",
+    "zlib",
+]
 
-lib_ffmpeg_a = ["build/ffmpeg/" + a for a in [
-    'libavformat/libavformat.a',
-    'libavcodec/libavcodec.a',
-    'libswscale/libswscale.a',
-    'libavutil/libavutil.a'
-]]
 
-static_libs = [libraylib_a, liblua_a]
 
 srcs = glob.glob("src/**/*.cpp", recursive=True)
-srcs += [imgui_dir + "/" + f for f in ["imgui.cpp", "imgui_demo.cpp", "imgui_draw.cpp", "imgui_tables.cpp", "imgui_widgets.cpp"]]
-srcs += [rlimgui_dir + "/rlImGui.cpp"]
-srcs += [implot_dir + "/" + f for f in ["implot.cpp", "implot_items.cpp"]]
+srcs += ["lib/imgui/" + f for f in ["imgui.cpp", "imgui_demo.cpp", "imgui_draw.cpp", "imgui_tables.cpp", "imgui_widgets.cpp"]]
+srcs += ["lib/rlImGui/rlImGui.cpp"]
+srcs += ["lib/implot/" + f for f in ["implot.cpp", "implot_items.cpp"]]
 objs = [os.path.splitext(f)[0] + ".o" for f in srcs]
 
-include_dirs = ["-I" + d for d in [imgui_dir, raylib_dir, raylib_dir + "/../examples/shaders", rlimgui_dir, lua_dir, ffmpeg_dir, implot_dir]]
-cpp_flags = ["-std=c++11", "-pedantic", "-Wall"] + include_dirs
-cpp_flags_debug = cpp_flags + ["-g"]
-cpp_flags_release = cpp_flags + ["-DNDEBUG", "-Os"]
-
 writer = ninja_syntax.Writer(sys.stdout)
-writer.variable("cxx", os.getenv("CXX", "g++"))
-writer.variable("cpp_flags_debug", " ".join(cpp_flags_debug))
-writer.variable("cpp_flags_release", " ".join(cpp_flags_release))
-writer.rule("cpp_debug", "$cxx $cpp_flags_debug -MD -MF $out.d $cflags -c -o $out $in", depfile="$out.d")
-writer.rule("cpp_release", "$cxx $cpp_flags_release -MD -MF $out.d $cflags -c -o $out $in", depfile="$out.d")
-writer.rule("link", "$cxx -o $out $in $ldflags")
-writer.rule("make", "make -j -C $dir $target")
 
-writer.rule("configure", "mkdir -p $build_dir && cd $build_dir && $cmd $flags", generator=True)
 
-writer.build("build/ffmpeg/Makefile", "configure", inputs=None, variables={"build_dir": "build/ffmpeg", "cmd": "../../lib/ffmpeg-8.0/configure", "flags": ffmpeg_configure_flags})
-writer.build(lib_ffmpeg_a, "make", variables={"dir": "build/ffmpeg"}, implicit="build/ffmpeg/Makefile")
+for target_name, target in targets.items():
+    raylib_build_dir = f"build/{target_name}/raylib/src"
+    lua_build_dir = f"build/{target_name}/lua-5.4.8/src"
+    ffmpeg_build_dir = f"build/{target_name}/ffmpeg-8.0"
+    zlib_build_dir = f"build/{target_name}/zlib"
 
-for src, obj in zip(srcs, objs):
-    writer.build("build/debug/" + obj, "cpp_debug", src)
-    writer.build("build/release/" + obj, "cpp_release", src)
-writer.build("bin/debug/rinkin", "link", ["build/debug/" + o for o in objs] + [libraylib_a, liblua_a] + lib_ffmpeg_a + [libz_a])
-writer.build("bin/release/rinkin", "link", ["build/release/" + o for o in objs] + [libraylib_a, liblua_a] + lib_ffmpeg_a + [libz_a])
+    libraylib_a = raylib_build_dir + "/libraylib.a"
+    liblua_a = lua_build_dir + "/liblua.a"
+    lib_ffmpeg_a = [f"build/{target_name}/ffmpeg-8.0/" + a for a in [
+        'libavformat/libavformat.a',
+        'libavcodec/libavcodec.a',
+        'libswscale/libswscale.a',
+        'libavutil/libavutil.a'
+    ]]
+    libz_a = zlib_build_dir + "/libz.a"
+    static_libs = [libraylib_a, liblua_a] + lib_ffmpeg_a + [libz_a] # the order is important for ffmpeg and zlib
 
-writer.build(libraylib_a, "make", variables={"dir": raylib_dir, "target": "PLATFORM=PLATFORM_DESKTOP"})
-writer.build(liblua_a, "make", variables={"dir": lua_dir, "target": "linux"})
+    include_dirs = ["-I" + d for d in ["lib/imgui/", "lib/raylib/src", "lib/raylib/examples/shaders", "lib/rlImGui", "lib/lua-5.4.8/src", "lib/ffmpeg-8.0", ffmpeg_build_dir, "lib/implot"]]
+    cpp_flags = ["-std=c++11", "-pedantic", "-Wall"] + include_dirs
+    cpp_flags_debug = cpp_flags + ["-g"]
+    cpp_flags_release = cpp_flags + ["-DNDEBUG", "-Os"]
+    #writer.variable("cpp_flags_debug", " ".join(cpp_flags_debug))
+    writer.variable(f"cpp_flags_{target_name}", " ".join(cpp_flags_release))
 
-writer.build("build/zlib/Makefile", "configure", variables={"build_dir": "build/zlib", "cmd": "../../lib/zlib/configure", "flags": ""})
-writer.build(libz_a, "make", variables={"dir": "build/zlib"}, implicit="build/zlib/Makefile")
+    try:
+        cc = f"{target['prefix']}gcc"
+        cxx = f"{target['prefix']}g++"
+        ar = f"{target['prefix']}ar"
+        ranlib = f"{target['prefix']}ranlib"
+        writer.variable(f"cc_{target_name}", cc)
+        writer.variable(f"cxx_{target_name}", cxx)
+    except KeyError:
+        cc = "gcc"
+        cxx = "g++"
+        ar = "ar"
+        ranlib = "ranlib"
+        writer.variable(f"cc_{target_name}", cc)
+        writer.variable(f"cxx_{target_name}", cxx)
+
+    
+
+    
+
+    #writer.rule("cpp_debug", "$cxx $cpp_flags_debug -MD -MF $out.d $cflags -c -o $out $in", depfile="$out.d")
+    writer.rule(f"cpp_release_{target_name}", f"$cxx_{target_name} $cpp_flags_{target_name} -MD -MF $out.d $cflags -c -o $out $in", depfile="$out.d")
+    writer.rule(f"link_{target_name}", f"$cxx_{target_name} -o $out $in $ldflags")
+    writer.rule(f"make_{target_name}", f"make CC={cc} CXX={cxx} $makefile $ar RANLIB={ranlib} -j4 -C $dir $target $options")
+    writer.rule(f"configure_{target_name}", "mkdir -p $build_dir && cd $build_dir && $env_vars $cmd $flags", generator=True)
+    writer.rule(f"copy_{target_name}", f"cp -r $in build/{target_name}")
+
+    writer.build(raylib_build_dir, f"copy_{target_name}", "lib/raylib")
+    writer.build(lua_build_dir, f"copy_{target_name}", "lib/lua-5.4.8")
+
+    configure_flags = " ".join(ffmpeg_configure_flags + target["ffmpeg-extra-flags"])
+    writer.build(f"{ffmpeg_build_dir}/Makefile", f"configure_{target_name}", inputs=None, variables={"build_dir": ffmpeg_build_dir, "cmd": "../../../lib/ffmpeg-8.0/configure", "flags": configure_flags})
+    writer.build(lib_ffmpeg_a, f"make_{target_name}", variables={"dir": ffmpeg_build_dir}, implicit=f"{ffmpeg_build_dir}/Makefile")
+
+    for src, obj in zip(srcs, objs):
+        #writer.build("build/debug/" + obj, "cpp_debug", src)
+        writer.build(f"build/{target_name}/" + obj, f"cpp_release_{target_name}", src, implicit=static_libs)
+    #writer.build("bin/debug/rinkin", "link", ["build/debug/" + o for o in objs] + [libraylib_a, liblua_a] + lib_ffmpeg_a + [libz_a])
+    writer.build(f"bin/{target_name}/rinkin", f"link_{target_name}", [f"build/{target_name}/" + o for o in objs] + static_libs, variables={"ldflags": " ".join(target["ldflags"])})
+
+    makefile_options = ["PLATFORM=PLATFORM_DESKTOP"]
+    if target_name == "windows":
+        makefile_options.append("PLATFORM_OS=WINDOWS")
+    elif target_name == "rpi":
+        makefile_options.append("GRAPHICS=GRAPHICS_API_OPENGL_21")
+    writer.build(libraylib_a, f"make_{target_name}", variables={"dir": raylib_build_dir, "target": " ".join(makefile_options)}, implicit=raylib_build_dir)
+    writer.build(liblua_a, f"make_{target_name}", variables={"dir": lua_build_dir, "target": target["lua-target"], "ar": f'AR="{ar} rcu"'}, implicit=lua_build_dir)
+
+    # zlib
+
+    if target_name != "windows":
+        try:
+            writer.build(f"{zlib_build_dir}/Makefile", f"configure_{target_name}", variables={"build_dir": zlib_build_dir, "env_vars": f"CROSS_PREFIX={target['prefix']}", "cmd": "../../../lib/zlib/configure", "flags": ""})
+        except KeyError:
+            writer.build(f"{zlib_build_dir}/Makefile", f"configure_{target_name}", variables={"build_dir": zlib_build_dir, "cmd": "../../../lib/zlib/configure", "flags": ""})
+        writer.build(libz_a, f"make_{target_name}", variables={"dir": zlib_build_dir}, implicit=f"{zlib_build_dir}/Makefile")
+    else:
+        writer.build(zlib_build_dir, f"copy_{target_name}", "lib/zlib")
+        writer.build(libz_a, f"make_{target_name}", variables={"dir": zlib_build_dir, "makefile": "-f win32/Makefile.gcc", "options": f"PREFIX={target['prefix']}"}, implicit=zlib_build_dir)
