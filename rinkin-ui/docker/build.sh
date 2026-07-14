6d67a8f50862258254a603583174b7159b76ce0c
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2026-07-14 21:50:02 +0200

    Dockerfile

--- rinkin-ui/docker/build.sh
+++ rinkin-ui/docker/build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-pushd ../../..
-pwd
-ls
-docker build -t rinkin -f RINKIN-ROV/rinkin-ui/docker/Dockerfile .
-docker create --name tmp rinkin
-docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin-linux-x86_64 RINKIN-ROV/rinkin-ui/docker
-docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin.exe RINKIN-ROV/rinkin-ui/docker
-docker cp tmp:/work/RINKIN-ROV/rinkin-ui/output/rinkin-raspberry-pi RINKIN-ROV/rinkin-ui/docker
-docker rm tmp
-
-popd
+#pushd ../..
+#pwd
+#ls
+#docker build -f rinkin-ui/docker/Dockerfile --output type=local,dest=./rinkin-ui/docker/out .
+docker build --output type=local,dest=./out .
